db528f80525fe2f36d3f6be67e513eb1062d4332
awhelan <andrew.whelan@bioraft.com>
2020-08-30 20:58:37 -0400

    Changing back to port 3k

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // read .env files
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 2999;
+const port = process.env.PORT || 3000;
 
 // Set public folder as root
 app.use(express.static('public'));
