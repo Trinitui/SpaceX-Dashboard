05ae4faa35b4765ade736824fccb6416340285c3
awhelan <andrew.whelan@bioraft.com>
2020-08-31 21:12:39 -0400

    getting funky!

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,13 @@ window.addEventListener('load', () => {
     const el = $('#app');
 
     // Complile HandleBar Templates (What does this mean???)
-    const dashboardTemplate = Handlebars.compile($('#dash-template').html());
     const errorTemplate = Handlebars.compile($('#error-template').html());
+    const dashboardTemplate = Handlebars.compile($('#dash-template').html());
     const lastTemplate = Handlebars.compile($('#last-launch-template').html());
     const upcomingTemplate = Handlebars.compile($('#upcoming-launch-template').html());
 
-    const html = dashboardTemplate();
+    const html = lastTemplate();
     el.html(html);
+    const html1 = dashboardTemplate();
+    el.html(html1)
 });
