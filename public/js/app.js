1ea9f4e1614f6460a7f6cba6f11d752ab205c866
awhelan <andrew.whelan@bioraft.com>
2020-08-31 21:06:26 -0400

    Adding a path to see if that works

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     const el = $('#app');
 
     // Complile HandleBar Templates (What does this mean???)
-    const dashboardTemplate = Handlebars.compile($('#dashboard-template').html())
+    const dashboardTemplate = Handlebars.compile($('#dashboard-template').html());
     const errorTemplate = Handlebars.compile($('#error-template').html());
     const lastTemplate = Handlebars.compile($('#last-launch-template').html());
     const upcomingTemplate = Handlebars.compile($('#upcoming-launch-template').html());
