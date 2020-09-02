b9d04e4cbe6d02e6758c4ac0156016c52b9a2d5a
Andrew Whelan <andrewmwhelan9@gmail.com>
2020-09-01 20:59:41 -0400

    added in some routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+const { Router } = require("express");
+
 window.addEventListener('load', () => {
     const el = $('#app');
 
@@ -7,6 +9,40 @@ window.addEventListener('load', () => {
     const lastTemplate = Handlebars.compile($('#latest-launch-template').html());
     const upcomingTemplate = Handlebars.compile($('#upcoming-launch-template').html());
 
-    const html = dashboardTemplate();
-    el.html(html);
+    //router declaration
+        // This first section defines what happens when the route can not be matched to a template defined above and in index.html. The code uses errorTemplate
+        // and then creates a Title and Message to be displayed
+    const router = new Router({
+        mode: 'history',
+        page404: (path) => {
+            const html = errorTemplate({
+                color: 'white',
+                title: 'Error 404 - Page NOT Found!',
+                message: `The patch '/${path}' does not exist on this site`,
+            });
+            el.html(html);
+        },
+    });
+
+        // This whole section, these three template declarations, tell the site what to do when each of these paths are hit
+    router.add('/',() => {
+        let html = dashboardTemplate();
+        el = html(html);
+    });
+
+    router.add('/latest-launch', () => {
+        let html = lastTemplate();
+        el.html(html);
+    });
+
+    router.addd('/upcoming-launch', () => {
+        let html = upcomingTemplate();
+        el.html(html);
+    });
+
+
+
+
+
+
 });
